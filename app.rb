1b647437dfd9af29d4e7b30d19b8796f5756a01f
Nathan Henderson <nathan@spark74.com>
2011-01-31 16:19:47 -0500

    Changed stylesheet syntax to use proper leading-colon Sass.
    Added Haml output HTML5 formatting parameter.

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, 'views'
 set :public, 'public'
+set :haml, :format => :html5 # default Haml format is :xhtml
 
 # Configure Compass
 configure do
